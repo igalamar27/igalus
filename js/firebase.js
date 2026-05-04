8d504be1c7e8f728c866209211b61786ddfa18c0
igalamar27 <igalamar27@gmail.com>
2026-05-04 15:06:59 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,19 +1,13 @@
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-auth-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js"></script>
-
-<script>
-  const firebaseConfig = {
-    apiKey: "TON_API_KEY",
-    authDomain: "TON_DOMAINE",
-    projectId: "igalus",
-    storageBucket: "TON_BUCKET",
-    messagingSenderId: "TON_ID",
-    appId: "TON_APP_ID"
-  };
-
-  firebase.initializeApp(firebaseConfig);
-
-  const auth = firebase.auth();
-  const db = firebase.firestore();
-</script>
+// Exemple (remplace par le tien)
+const firebaseConfig = {
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "igalus",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "..."
+};
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+``
